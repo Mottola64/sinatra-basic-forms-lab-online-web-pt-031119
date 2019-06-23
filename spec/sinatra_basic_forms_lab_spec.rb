7c699bca06645dc8cb2951bd166bc641e67ec846
Michael Mottola <mottola64@gmail.com>
2019-06-23 23:11:25 +0000

    Automatically backed up by Learn

--- spec/sinatra_basic_forms_lab_spec.rb
+++ spec/sinatra_basic_forms_lab_spec.rb
@@ -35,7 +35,7 @@ describe App do
 
       expect(page).to have_field(:name)
       expect(page).to have_field(:breed)
-      expect(page).to have_field(:age)
+      expect(page).to have_field(:months_old)
     end
   end
 
@@ -46,7 +46,7 @@ describe App do
 
       fill_in(:name, :with => "Butch")
       fill_in(:breed, :with => "Mastiff")
-      fill_in(:age, :with => "6")
+      fill_in(:months_old, :with => "6")
 
       #the below css will match any element (input or button)
       #with a type attribute set to submit
@@ -65,14 +65,14 @@ describe App do
       # The \s below will match any whitespace
       expect(page).to have_text(/Name:\s+Butch/i)
       expect(page).to have_text(/Breed:\s+Mastiff/i)
-      expect(page).to have_text(/Age:\s+6 months/i)
+      expect(page).to have_text(/Months_old:\s+6 months/i)
 
       #now do it again, to be sure it's not hard-coded
       visit '/new'
 
       fill_in(:name, :with => "Byron")
       fill_in(:breed, :with => "Poodle")
-      fill_in(:age, :with => "9")
+      fill_in(:months_old, :with => "9")
 
       #the below css will match any element (input or button)
       #with a type attribute set to submit
@@ -80,7 +80,7 @@ describe App do
 
       expect(page).to have_text(/Name:\s+Byron/i)
       expect(page).to have_text(/Breed:\s+Poodle/i)
-      expect(page).to have_text(/Age:\s+9 months/i)
+      expect(page).to have_text(/Months_old:\s+9 months/i)
     end
   end
 
